5059cd83438183426d266616f94b247835b29e77
Claudia <claudi_a14@hotmail.com>
2018-06-11 19:13:28 -0400

    Agregando  Test  a calculateStore

--- src/test/java/CalculateScoreTest.java
+++ src/test/java/CalculateScoreTest.java
@@ -13,6 +13,80 @@ import org.junit.Test;
 public class CalculateScoreTest {	
 	
 	//Pruebas calculateScores
+	@Test
+	public void testCalculateScores2NegativoWordHave() {
+		List<Sentence> Lista= new ArrayList<Sentence>();
+		Sentence sentence= new Sentence(-2, "I would have preferred an easier assignment");
+		Lista.add(sentence);
+		
+		Set<Word> ListaPalabrasResult=new TreeSet<Word>();
+		Word wordHave=new Word("have");
+		wordHave.increaseTotal(-2);
+		ListaPalabrasResult.add(wordHave);
+		
+		 Map<String, Double> map = new HashMap<String, Double>();
+	        map = Analyzer.calculateScores(ListaPalabrasResult);
+
+	        double scoreResult = 0;
+	        for (Map.Entry<String, Double> entry : map.entrySet()) {
+	            if ("have".equals(entry.getKey())) {
+	            	scoreResult = entry.getValue();
+	            }
+	        }
+	        double scoreEsperado=-2.0;
+	        assertEquals(String.valueOf(scoreEsperado), String.valueOf(scoreResult));
+	}
+	@Test
+	public void testCalculateScores1Punto33WordNot() {
+		
+		Set<Word> ListaPalabrasResult=new TreeSet<Word>();
+		Word wordNot=new Word("not");
+		wordNot.increaseTotal(2);
+		wordNot.increaseTotal(2);
+		wordNot.increaseTotal(0);
+		ListaPalabrasResult.add(wordNot);
+		int numeroRepetiicones=3;
+		double sumaValores=4;
+		
+		
+		 Map<String, Double> map = new HashMap<String, Double>();
+	        map = Analyzer.calculateScores(ListaPalabrasResult);
+
+	        double scoreResult = 0;
+	        for (Map.Entry<String, Double> entry : map.entrySet()) {
+	            if ("not".equals(entry.getKey())) {
+	            	scoreResult = entry.getValue();
+	            }
+	        }
+	        double scoreEsperado=(double)sumaValores/numeroRepetiicones;
+	        System.out.println(scoreResult);
+	        assertEquals(String.valueOf(scoreEsperado), String.valueOf(scoreResult));
+	}
+	@Test
+	public void testCalculateScoresWordNotExistReturnCero() {
+		List<Sentence> Lista= new ArrayList<Sentence>();
+		Sentence sentence= new Sentence(-2, "I would have preferred an easier assignment");
+		Lista.add(sentence);
+		
+		Set<Word> ListaPalabrasResult=new TreeSet<Word>();
+		/*Word wordHave=new Word("have");
+		wordHave.increaseTotal(-2);
+		ListaPalabrasResult.add(wordHave);
+		*/
+		 Map<String, Double> map = new HashMap<String, Double>();
+	        map = Analyzer.calculateScores(ListaPalabrasResult);
+
+	        double scoreResult = 0;
+	        for (Map.Entry<String, Double> entry : map.entrySet()) {
+	            if ("have".equals(entry.getKey())) {
+	            	scoreResult = entry.getValue();
+	            }
+	        }
+	        double scoreEsperado=0;
+	        assertEquals(String.valueOf(scoreEsperado), String.valueOf(scoreResult));
+	}
+	
+	//Serian Pruebas de integracion porque utliza las otras funciones en su calculo
 	
 	  @Test
 	    public void testCalculateScores1WordMuch() {
@@ -33,14 +107,16 @@ public class CalculateScoreTest {
 	        double scoreEsperado=1.0;
 	        assertEquals(String.valueOf(scoreEsperado), String.valueOf(scoreResult));
 	    }
+	  
 	  @Test
-		public void testCalculateScores2NegativowouldThis() {
+		public void testCalculateScores2NegativoWordWould() {
 			List<Sentence> Lista= new ArrayList<Sentence>();
 			Sentence sentence= new Sentence(-2, "I would have preferred an easier assignment");
 			Lista.add(sentence);
 			Set<Word> ListaPalabrasResult=Analyzer.allWords(Lista);
 			 Map<String, Double> map = new HashMap<String, Double>();
 		        map = Analyzer.calculateScores(ListaPalabrasResult);
+		        System.out.println(map);
 
 		        double scoreResult = 0;
 		        for (Map.Entry<String, Double> entry : map.entrySet()) {
@@ -51,6 +127,7 @@ public class CalculateScoreTest {
 		        double scoreEsperado=-2.0;
 		        assertEquals(String.valueOf(scoreEsperado), String.valueOf(scoreResult));
 		}
+	  
 	  @Test
 	    public void testCalculateScores05WordThis() {
 	        Set<Word> ListaPalabrasResult = new TreeSet<Word>();
